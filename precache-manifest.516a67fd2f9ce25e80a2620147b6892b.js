d94b5e7ffe921011138f4313b5bec1ce73352692
danielcaveman <daniel_caverna@yahoo.com.br>
2020-08-18 10:56:45 -0300

    Updates

---
+++ precache-manifest.516a67fd2f9ce25e80a2620147b6892b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e06855911ecf25791bb4e67a533efe8",
+    "url": "/react-dex/index.html"
+  },
+  {
+    "revision": "4d1efa563bccbd532e44",
+    "url": "/react-dex/static/js/2.34ec59d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-dex/static/js/2.34ec59d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc96f40d278bb6e1c3b",
+    "url": "/react-dex/static/js/main.d7564b58.chunk.js"
+  },
+  {
+    "revision": "3f56585895e44089bd1f",
+    "url": "/react-dex/static/js/runtime-main.d295d334.js"
+  }
+]);
